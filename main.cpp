acab7431241f1890f56d1bbd3dab1a86484ff675
qwerchik9 <156547238+qwerchik9@users.noreply.github.com>
2024-02-12 09:49:14 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int umn(int a, int b) {
+	return a * b;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << umn(a, b) << " " << raz(a, b) << " " << sum(a, b);
+}
